090ac1963c58da4afba1b1608deb62abe1795735
FOUREX <76780880+FOUREX@users.noreply.github.com>
2023-06-18 18:13:15 +0300

    Delete duck directory

--- duck/welcome.cpp
+++
@@ -1,9 +0,0 @@
-#include "welcome.h"
-#include "yep.h"
-
-void duck::welcome::loginButton_Click(System::Object^ sender, System::EventArgs^ e)
-{
-	duck::yep^ form = gcnew duck::yep();
-	form->Show();
-	this->Hide();
-}
